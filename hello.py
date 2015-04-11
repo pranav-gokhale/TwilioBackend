1f4e013b8f717d14ad09fba80d7c03a30a903b8c
Pranav Maddi <pranav.maddi@gmail.com>
2015-04-11 14:14:58 +0400

    Added first text and framework for sending data to parse

--- hello.py
+++ hello.py
@@ -3,13 +3,36 @@ import twilio.twiml
  
 app = Flask(__name__)
  
+FIRST_TEXT = '''
+Press 1 for English, 2 for Amharic, 3 for Bengali, 4 for Hindi, 5 for Indonesian, 6 for Malayalam, 7 for Mandarin, 8 for Nepali, 9 for Sinhalese, 10 for Tagalog, 11 for Tamil, 12 for Telugu
+'''
+
 @app.route("/", methods=['GET', 'POST'])
 def hello_monkey():
-    """Respond to incoming requests."""
+
     resp = twilio.twiml.Response()
-    resp.say("Hello Monkey")
+    resp.say(FIRST_TEXT)
  
     return str(resp)
  
+def send_data():
+	import json,httplib
+	connection = httplib.HTTPSConnection('api.parse.com', 443)
+	connection.connect()
+	connection.request('POST', '/1/classes/GameScore', json.dumps({
+	       "score": 1337,
+	       "playerName": "Sean Plott",
+	       "cheatMode": False
+	     }), {
+	       "X-Parse-Application-Id": "2W6rB0trZRZNa0jyrcbvFGoI8yN7PXqs8L6z4DQi",
+	       "X-Parse-REST-API-Key": "kK8riCXFGptYwPbrc100DSxFBe4aAijY1OctNEF6",
+	       "Content-Type": "application/json"
+	     })
+	result = json.loads(connection.getresponse().read())
+	print result
+
+
+
 if __name__ == "__main__":
     app.run(debug=True)
+
