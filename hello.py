3982dc4d5ce9028c887f089bd57acec2550017ab
Pranav Maddi <pranav.maddi@gmail.com>
2015-04-11 14:25:03 +0400

    Save user phone number

--- hello.py
+++ hello.py
@@ -3,27 +3,32 @@ import twilio.twiml
  
 app = Flask(__name__)
  
-FIRST_TEXT = '''
+INTRO_TEXT = '''
 Press 1 for English, 2 for Amharic, 3 for Bengali, 4 for Hindi, 5 for Indonesian, 6 for Malayalam, 7 for Mandarin, 8 for Nepali, 9 for Sinhalese, 10 for Tagalog, 11 for Tamil, 12 for Telugu
 '''
 
 @app.route("/", methods=['GET', 'POST'])
 def hello_monkey():
 
+	'''debugging'''
+	print request
+
+	'''Save data about user'''
+	data_blob = {}
+	data_blob["from_number"] = request.values.get('From', None)
+	send_data(data_blob)
+
+	'''Interact with user'''
     resp = twilio.twiml.Response()
-    resp.say(FIRST_TEXT)
+    resp.say(INTRO_TEXT)
  
     return str(resp)
  
-def send_data():
+def send_data(blob):
 	import json,httplib
 	connection = httplib.HTTPSConnection('api.parse.com', 443)
 	connection.connect()
-	connection.request('POST', '/1/classes/GameScore', json.dumps({
-	       "score": 1337,
-	       "playerName": "Sean Plott",
-	       "cheatMode": False
-	     }), {
+	connection.request('POST', '/1/classes/GameScore', json.dumps(blob), {
 	       "X-Parse-Application-Id": "2W6rB0trZRZNa0jyrcbvFGoI8yN7PXqs8L6z4DQi",
 	       "X-Parse-REST-API-Key": "kK8riCXFGptYwPbrc100DSxFBe4aAijY1OctNEF6",
 	       "Content-Type": "application/json"
